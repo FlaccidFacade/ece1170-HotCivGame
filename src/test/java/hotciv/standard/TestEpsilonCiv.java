dadf939b07e0e6ec9b0519f30bbca9a4b78492d5
Doraonroids <zgt4@pitt.edu>
2021-03-26 00:02:24 -0400

    Create TestEpsilonCiv.java
    Create a test file for new variant

---
+++ src/test/java/hotciv/standard/TestEpsilonCiv.java
@@ -0,0 +1,21 @@
+package hotciv.standard;
+
+import hotciv.framework.*;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.*;
+
+public class TestEpsilonCiv {
+
+    @Before
+    public void setUp() {
+
+        
+    }
+
+
+}
