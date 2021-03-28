1685325dce59c7e925b71d9fc127aa0494eba107
Doraonroids <zgt4@pitt.edu>
2021-03-28 06:13:13 -0400

    repeat factories
    all factories for all variants work

---
+++ src/main/java/hotciv/standard/Factories/GammaCivFactory.java
@@ -0,0 +1,37 @@
+package hotciv.standard.Factories;
+
+import hotciv.framework.Factories.HotCivFactory;
+import hotciv.framework.Strategies.*;
+import hotciv.standard.Strategies.*;
+
+public class GammaCivFactory implements HotCivFactory {
+    @Override
+    public ActionStrategy createActionStrategy() {
+        return new GammaActionStrategy();
+    }
+
+    @Override
+    public AgingStrategy createAgingStrategy() {
+        return new AlphaAgingStrategy();
+    }
+
+    @Override
+    public BattleStrategy createBattleStrategy() {
+        return new AlphaBattleStrategy();
+    }
+
+    @Override
+    public GrowthStrategy createGrowthStrategy() {
+        return new AlphaGrowthStrategy();
+    }
+
+    @Override
+    public ProductionStrategy createProductionStrategy() {
+        return new AlphaProductionStrategy();
+    }
+
+    @Override
+    public WinningStrategy createWinningStrategy() {
+        return new AlphaWinningStrategy();
+    }
+}
