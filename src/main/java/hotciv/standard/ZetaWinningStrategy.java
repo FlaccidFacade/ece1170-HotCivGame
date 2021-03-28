9fc0e310c36a4dbb7d7f5ea98d87ce63acdf37eb
Doraonroids <zgt4@pitt.edu>
2021-03-28 04:52:35 -0400

    refactor zeta
    use state design. this approach is a strategy with two other strategies inside

--- src/main/java/hotciv/standard/ZetaWinningStrategy.java
+++ src/main/java/hotciv/standard/ZetaWinningStrategy.java
@@ -7,26 +7,31 @@ import java.util.Map;
 
 public class ZetaWinningStrategy implements WinningStrategy {
 
+    private final WinningStrategy betaWinningStrategy = new BetaWinningStrategy();
+    private final WinningStrategy epsilonWinningStrategy = new EpsilonWinningStrategy();
+
     @Override
     public Player getWinner(int age, World world, Map<Player, Integer> playerIntegerMap, int round) {
-        List<City> cityList = world.getAllCities();
-        Player winner = cityList.get(0).getOwner();
+//        List<City> cityList = world.getAllCities();
+//        Player winner = cityList.get(0).getOwner();
         if(round < 19) {
+            return betaWinningStrategy.getWinner(age,world,playerIntegerMap,round);
+//            for (City c : cityList) {
+//                if (c.getOwner() != winner) {
+//                    return null;
+//                }
+//            }
 
-            for (City c : cityList) {
-                if (c.getOwner() != winner) {
-                    return null;
-                }
-            }
         }else{
-            for (Map.Entry<Player, Integer> entry : playerIntegerMap.entrySet()) {
-                if (entry.getValue() == 3) {
-                    return entry.getKey();
-                }
-            }
+            return epsilonWinningStrategy.getWinner(age,world,playerIntegerMap,round);
+//            for (Map.Entry<Player, Integer> entry : playerIntegerMap.entrySet()) {
+//                if (entry.getValue() == 3) {
+//                    return entry.getKey();
+//                }
+//            }
         }
 
 
-        return winner;
+        //return winner;
     }
 }
