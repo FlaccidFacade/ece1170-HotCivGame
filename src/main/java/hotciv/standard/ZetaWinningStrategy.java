7eea10d065c60cca243bdf12dc224430c2b85687
Doraonroids <zgt4@pitt.edu>
2021-03-28 05:20:29 -0400

    Update ZetaWinningStrategy.java
    remove unused code

--- src/main/java/hotciv/standard/ZetaWinningStrategy.java
+++ src/main/java/hotciv/standard/ZetaWinningStrategy.java
@@ -2,7 +2,6 @@ package hotciv.standard;
 
 import hotciv.framework.*;
 
-import java.util.List;
 import java.util.Map;
 
 public class ZetaWinningStrategy implements WinningStrategy {
@@ -12,26 +11,12 @@ public class ZetaWinningStrategy implements WinningStrategy {
 
     @Override
     public Player getWinner(int age, World world, Map<Player, Integer> playerIntegerMap, int round) {
-//        List<City> cityList = world.getAllCities();
-//        Player winner = cityList.get(0).getOwner();
         if(round < 19) {
             return betaWinningStrategy.getWinner(age,world,playerIntegerMap,round);
-//            for (City c : cityList) {
-//                if (c.getOwner() != winner) {
-//                    return null;
-//                }
-//            }
 
-        }else{
-            return epsilonWinningStrategy.getWinner(age,world,playerIntegerMap,round);
-//            for (Map.Entry<Player, Integer> entry : playerIntegerMap.entrySet()) {
-//                if (entry.getValue() == 3) {
-//                    return entry.getKey();
-//                }
-//            }
+
+        }else {
+            return epsilonWinningStrategy.getWinner(age, world, playerIntegerMap, round);
         }
-
-
-        //return winner;
     }
 }
