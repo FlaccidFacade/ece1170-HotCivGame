e7b5933cdc3b9096c6d693673865469d488a4f44
Doraonroids <zgt4@pitt.edu>
2021-04-26 06:49:26 -0400

    Action Tool

--- src/main/java/hotciv/visual/ShowAction.java
+++ src/main/java/hotciv/visual/ShowAction.java
@@ -1,6 +1,13 @@
 package hotciv.visual;
 
 import hotciv.framework.Game;
+import hotciv.framework.GameConstants;
+import hotciv.framework.Player;
+import hotciv.framework.Position;
+import hotciv.standard.CityImpl;
+import hotciv.standard.GameImpl;
+import hotciv.standard.UnitImpl;
+import hotciv.standard.tools.ActionTool;
 import hotciv.stub.StubGame2;
 import minidraw.framework.DrawingEditor;
 import minidraw.standard.MiniDrawApplication;
@@ -25,7 +32,19 @@ import minidraw.standard.NullTool;
 public class ShowAction {
   
   public static void main(String[] args) {
-    Game game = new StubGame2();
+    Game game = new GameImpl();
+    game.placeUnitAt( new Position( 2,0), new UnitImpl(Player.RED, GameConstants.ARCHER));
+    game.placeUnitAt( new Position( 2,1), new UnitImpl(Player.RED, GameConstants.ARCHER));
+
+
+    game.placeUnitAt( new Position( 3,2), new UnitImpl(Player.BLUE, GameConstants.LEGION));
+
+    game.placeUnitAt( new Position( 4,3), new UnitImpl(Player.RED, GameConstants.SETTLER));
+
+    game.placeUnitAt( new Position( 6,4), new UnitImpl(Player.RED, GameConstants.UFO));
+
+    game.placeCityAt( new Position( 6,6), new CityImpl(Player.RED));
+    game.placeCityAt( new Position( 9,9), new CityImpl(Player.BLUE));
 
     DrawingEditor editor = 
       new MiniDrawApplication( "Shift-Click unit to invoke its action",  
@@ -33,7 +52,6 @@ public class ShowAction {
     editor.open();
     editor.showStatus("Shift-Click on unit to see Game's performAction method being called.");
 
-    // TODO: Replace the setting of the tool with your ActionTool implementation.
-    editor.setTool( new NullTool() );
+    editor.setTool( new ActionTool(editor,game) );
   }
 }
